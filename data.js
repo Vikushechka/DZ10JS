bcede0587bebc2908b944725bd55f45b9c125af9
«Viktoriya <catwomen.07@mail.ru>
2023-03-17 02:04:31 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,30 @@
+const data = `[
+    {
+        "id" : "1",
+        "activity" : "Pull a harmless prank on one of your friends",
+        "type": "social",
+        "participants": 1,
+        "img" : "https://images.dog.ceo/breeds/malamute/n02110063_11227.jpg"
+      },
+      {
+        "id" : "2",
+        "activity": "Learn how to make a website",
+        "type": "education",
+        "participants": 1,
+        "img" : "https://images.dog.ceo/breeds/appenzeller/n02107908_599.jpg"
+      },
+      {
+        "id" : "3",
+        "activity": "Watch a classic movie",
+        "type": "recreational",
+        "participants": 1,
+        "img" : "https://images.dog.ceo/breeds/retriever-curly/n02099429_1627.jpg"
+      },
+      {
+        "id" : "4",
+        "activity": "Research a topic you're interested in",
+        "type": "education",
+        "participants": 1,
+        "img" : "https://images.dog.ceo/breeds/terrier-norwich/n02094258_1291.jpg"
+      }
+]`
